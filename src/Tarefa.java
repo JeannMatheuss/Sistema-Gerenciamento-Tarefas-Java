18ff4c1f3ce83fdb66e88cb2dc6898e14bb63250
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-13 22:00:51 -0300

    Criando os métodos getters e setters dos atributos da classe Tarefa

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -6,4 +6,38 @@ public class Tarefa {
     private String descricao;
     private LocalDate dataVencimento;
     private Boolean status;
+
+    
+    public int getID() {
+        return ID;
+    }
+    public void setID(int iD) {
+        ID = iD;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+    public Boolean getStatus() {
+        return status;
+    }
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    
 }
